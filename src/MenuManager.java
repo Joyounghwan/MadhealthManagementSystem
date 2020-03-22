8b7a0e8dbf660b0bdd559cf32208718464b8abde
aaaaa <aaaaa@DESKTOP-2216C2R>
2020-03-22 22:28:33 +0900

    Create a menu

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,10 +1,10 @@
-public class HelloWorld {
+public class MenuManager {
 	public static void main(String[] args) {
-		System.out.println("*** Student Management System Menu ***");
-		System.out.println(" 1. Add Student");
-		System.out.println(" 2. Delete Student");
-		System.out.println(" 3. Edit Student");
-		System.out.println(" 4. View Student");
+		System.out.println("*** Madhealth Management System Menu ***");
+		System.out.println(" 1. Stretching");
+		System.out.println(" 2. Bodybuilding");
+		System.out.println(" 3. Health tip");
+		System.out.println(" 4. Diet");
 		System.out.println(" 5. Exit");
 		System.out.println("Select one number between 1 - 5:");
 	}
